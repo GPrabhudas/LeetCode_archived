375d099fd279d4c9b0703fc2905e023f95c5f54e
prabhudas552 <garule.prabhudas@carwale.com>
2018-11-06 00:41:40 +0530

    add optimized version

--- Tree/maximum-binary-tree/solution_02.go
+++ Tree/maximum-binary-tree/solution_02.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 // TreeNode ...
@@ -11,31 +12,58 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
-
-func constructSegmentTree(arr, st []int, ss, se, si int) int {
-	if ss == se {
-		st[si] = ss
-		return st[si]
-	}
-	mid := ss + (se-ss)/2
-	left := constructSegmentTree(arr, st, ss, mid, 2*si+1)
-	right := constructSegmentTree(arr, st, mid+1, se, 2*si+2)
-	st[si] = left
-	if arr[right] > arr[left] {
-		st[si] = right
-	}
-	return st[si]
+type keyValue struct {
+	index int
+	value int
 }
+
 func main() {
-	arr := []int{3, 2, 1, 6, 0, 5}
-	st := make([]int, 2*len(arr)-1)
+	arr := []int{1, 9, 4, 6, 8, 7, 11, 10}
+	h := int(math.Log2(float64(len(arr)))) + 1
+	stsize := 2*int((math.Pow(2, float64(h)))) - 1
+	st := make([]keyValue, stsize)
 	constructSegmentTree(arr, st, 0, len(arr)-1, 0)
 	root := constructMaximumBinaryTree(arr, st)
 	inorder(root)
 	fmt.Println()
 }
 
-func constructMaximumBinaryTree(nums, st []int) *TreeNode {
+func max(a, b keyValue) keyValue {
+	if a.index == -1 {
+		return b
+	}
+	if b.index == -1 {
+		return a
+	}
+
+	if a.value > b.value {
+		return a
+	}
+	return b
+}
+
+func constructSegmentTree(arr []int, st []keyValue, ss, se, si int) keyValue {
+	if ss == se {
+		st[si] = keyValue{index: ss, value: arr[ss]}
+		return st[si]
+	}
+	mid := ss + (se-ss)/2
+	st[si] = max(constructSegmentTree(arr, st, ss, mid, 2*si+1), constructSegmentTree(arr, st, mid+1, se, 2*si+2))
+	return st[si]
+}
+
+func getMaxIndex(st []keyValue, ss, se, qs, qe, si int) keyValue {
+	if ss >= qs && se <= qe {
+		return st[si]
+	}
+	if se < qs || ss > qe {
+		return keyValue{index: -1, value: -1}
+	}
+	mid := ss + (se-ss)/2
+	return max(getMaxIndex(st, ss, mid, qs, qe, 2*si+1), getMaxIndex(st, mid+1, se, qs, qe, 2*si+2))
+}
+
+func constructMaximumBinaryTree(nums []int, st []keyValue) *TreeNode {
 	return construct(nums, st, 0, len(nums))
 }
 
@@ -46,34 +74,15 @@ func inorder(root *TreeNode) {
 		inorder(root.Right)
 	}
 }
-func construct(nums, st []int, l, r int) *TreeNode {
+
+func construct(nums []int, st []keyValue, l, r int) *TreeNode {
 	if l == r {
 		return nil
 	}
-	maxI := getMaxIndex(nums, st, 0, len(nums)-1, l, r, 0)
-	root := &TreeNode{
-		Val: nums[maxI],
-	}
+	maxNode := getMaxIndex(st, 0, len(nums)-1, l, r-1, 0)
+	maxI := maxNode.index
+	root := &TreeNode{Val: nums[maxI], Left: nil, Right: nil}
 	root.Left = construct(nums, st, l, maxI)
 	root.Right = construct(nums, st, maxI+1, r)
 	return root
-}
-
-func getMaxIndex(nums, st []int, ss, se, qs, qe, si int) int {
-	if qs <= ss && qe >= se {
-		return st[si]
-	}
-	if qe < ss || qs > se {
-		return len(nums)
-	}
-	mid := ss + (se-ss)/2
-	left := getMaxIndex(nums, st, ss, mid, qs, qe, 2*si+1)
-	right := getMaxIndex(nums, st, mid+1, se, qs, qe, 2*si+2)
-	if left == len(nums) {
-		return right
-	}
-	if right == len(nums) {
-		return left
-	}
-
 }
